a3309875a7a97c13559a6f7c45a67978233c8552
GIAHUY\GiaHuy <huypro99998@gmail.com>
2025-10-22 20:44:14 +0700

    add maildev test

--- utils/mail.js
+++ utils/mail.js
@@ -1,20 +1,33 @@
 const nodemailer = require('nodemailer');
 
-const sendMail = async ({ to, subject, text }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const sendMail = async ({ to, subject, text, html }) => {
+  let transporter;
+
+  if (process.env.NODE_ENV === 'production') {
+    transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      host: 'localhost',
+      port: 1025,
+      secure: false,
+    });
+  }
 
   await transporter.sendMail({
     from: '"Donation App" <no-reply@donation.com>',
     to,
     subject,
-    text
+    text,
+    html,
   });
+
+  console.log(`Email sent to ${to} (${process.env.NODE_ENV})`);
 };
 
 module.exports = { sendMail };
