801cf4902b82de9838b7778af6afe410c5047989
GIAHUY\GiaHuy <huypro99998@gmail.com>
2025-10-22 23:55:06 +0700

    add analyzesentiment use huggingface

--- services/feedbackService.js
+++ services/feedbackService.js
@@ -1,5 +1,27 @@
 const Feedback = require('../models/Feedback');
 const AppError = require('../utils/AppError');
+const axios = require('axios');
+
+const analyzeSentiment = async (text) => {
+  try {
+    const response = await axios.post(
+      "https://router.huggingface.co/hf-inference/models/cardiffnlp/twitter-roberta-base-sentiment",
+      { inputs: text },
+      {
+        headers: { Authorization: `Bearer ${process.env.HF_API_KEY}` },
+      }
+    );
+    const labelMap = {
+      LABEL_2: 'positive',
+      LABEL_1: 'neutral',
+      LABEL_0: 'negative'
+    };
+    const top = response.data[0][0];
+    return labelMap[top.label] || 'neutral';
+  } catch (error) {
+    return 'neutral';
+  }
+};
 
 const submitFeedback = async (feedbackData) => {
   try {
@@ -8,10 +30,13 @@ const submitFeedback = async (feedbackData) => {
       throw new AppError('Please provide all required fields', 400);
     }
 
+    const sentiment = await analyzeSentiment(comment);
+
     const feedback = await Feedback.create({
       userId,
       campaignId,
-      comment
+      comment,
+      sentiment
     });
 
     return feedback;
